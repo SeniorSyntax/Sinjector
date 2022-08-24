cd7f825602c878ad7ea81cb33a2cb8b894183ef5
Mathias Stenbom <mathias.stenbom@calabrio.com>
2022-08-24 13:27:50 +0200

    Simple sample

---
+++ Sinjector.Test/SampleTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Autofac;
+using NUnit.Framework;
+
+namespace Sinjector.Test;
+
+[SinjectorFixture]
+[MySystem]
+public class SampleTest
+{
+    public MyService Target;
+
+    [Test]
+    public void ShouldDoStuff()
+    {
+        Target.DoStuff();
+    }
+}
+
+public class MySystemAttribute : Attribute, IContainerSetup
+{
+    public void ContainerSetup(IContainerSetupContext context)
+    {
+        context.AddModule(new MySystemModule());
+    }
+}
+
+
+public class MySystemModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<MyService>();
+    }
+}
+
+public class MyService
+{
+    public void DoStuff()
+    {
+    }
+}
