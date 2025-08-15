def3bbb5b7c87cde032d3924850956802b0cccf4
Roger Kratz <roger.kratz@gmail.com>
2025-08-15 09:48:38 +0200

    minor refact

--- Sinjector/SinjectorFixtureAttribute.cs
+++ Sinjector/SinjectorFixtureAttribute.cs
@@ -7,136 +7,133 @@ using NUnit.Framework.Interfaces;
 using NUnit.Framework.Internal;
 using Sinjector.Internals;
 
-namespace Sinjector
+namespace Sinjector;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class SinjectorFixtureAttribute : Attribute, ITestAction, ISinjectorTestContext
 {
-	[AttributeUsage(AttributeTargets.Class)]
-	public class SinjectorFixtureAttribute : Attribute, ITestAction, ISinjectorTestContext
+	public ActionTargets Targets => ActionTargets.Test;
+
+	private Injector _injector;
+	private ExtensionQuerier _extensions;
+
+	private class TestState
 	{
-		public ActionTargets Targets => ActionTargets.Test;
+		public object Fixture;
+		public IContainer Container;
+		public ITestDoubles TestDoubles;
+	}
 
-		private Injector _injector;
-		private ExtensionQuerier _extensions;
+	private static string WorkerId => TestContext.CurrentContext.WorkerId ?? "single";
+	private readonly ConcurrentDictionary<string, TestState> _state = new();
+	private TestState State => _state[WorkerId];
 
-		private readonly ConcurrentDictionary<string, TestState> _state = new ConcurrentDictionary<string, TestState>();
+	public void BeforeTest(ITest testDetails)
+	{
+		_state[WorkerId] = new TestState();
+		State.Fixture = testDetails.Fixture;
+		State.TestDoubles = new TestDoubles();
+		_extensions = new ExtensionQuerier()
+			.Fixture(testDetails.Fixture)
+			.Attribute(this)
+			.TestMethod(testDetails.Method.MethodInfo);
+		buildContainer();
 
-		private class TestState
+		if (TestExecutionContext.CurrentContext.ParallelScope.HasFlag(ParallelScope.Children))
+			Injector.Inject(State.Fixture, this);
+		else
 		{
-			public object Fixture;
-			public IContainer Container;
-			public ITestDoubles TestDoubles;
+			_injector = new Injector()
+				.Source(State.Container)
+				.Target(testDetails.Fixture)
+				.Target(this);
+			_injector.Inject();
 		}
 
-		private static string WorkerId => TestContext.CurrentContext.WorkerId ?? "single";
-		private TestState State => _state[WorkerId];
+		InvokeExtensions<ITestSetup>(x => x.TestSetup());
+	}
 
-		public void BeforeTest(ITest testDetails)
+	private void buildContainer()
+	{
+		InvokeExtensions<IContainerBuild>(a =>
 		{
-			_state[WorkerId] = new TestState();
-			State.Fixture = testDetails.Fixture;
-			State.TestDoubles = new TestDoubles();
-			_extensions = new ExtensionQuerier()
-				.Fixture(testDetails.Fixture)
-				.Attribute(this)
-				.TestMethod(testDetails.Method.MethodInfo);
-			buildContainer();
-
-			if (TestExecutionContext.CurrentContext.ParallelScope.HasFlag(ParallelScope.Children))
-				Injector.Inject(State.Fixture, this);
-			else
+			State.Container = a.ContainerBuild(builder =>
 			{
-				_injector = new Injector()
-					.Source(State.Container)
-					.Target(testDetails.Fixture)
-					.Target(this);
-				_injector.Inject();
-			}
-
-			InvokeExtensions<ITestSetup>(x => x.TestSetup());
-		}
-
-		private void buildContainer()
-		{
-			InvokeExtensions<IContainerBuild>(a =>
-			{
-				State.Container = a.ContainerBuild(builder =>
-				{
-					register(builder, () => new ContainerAdaptor(State.TestDoubles, builder, _extensions));
-				});
-			});
-
-			if (State.Container == null)
-			{
-				var builder = new ContainerBuilder();
 				register(builder, () => new ContainerAdaptor(State.TestDoubles, builder, _extensions));
-				State.Container = builder.Build();
-			}
-		}
-
-		private void rebuildContainer()
-		{
-			State.Container = null;
-
-			InvokeExtensions<IContainerBuild>(a =>
-			{
-				State.Container = a.ContainerBuild(builder =>
-				{
-					register(builder, () => new ContainerAdaptor(new IgnoreTestDoubles(), builder, _extensions));
-					State.TestDoubles.RegisterFromPreviousContainer(builder);
-				});
 			});
+		});
 
-			if (State.Container == null)
+		if (State.Container == null)
+		{
+			var builder = new ContainerBuilder();
+			register(builder, () => new ContainerAdaptor(State.TestDoubles, builder, _extensions));
+			State.Container = builder.Build();
+		}
+	}
+
+	private void rebuildContainer()
+	{
+		State.Container = null;
+
+		InvokeExtensions<IContainerBuild>(a =>
+		{
+			State.Container = a.ContainerBuild(builder =>
 			{
-				var builder = new ContainerBuilder();
 				register(builder, () => new ContainerAdaptor(new IgnoreTestDoubles(), builder, _extensions));
 				State.TestDoubles.RegisterFromPreviousContainer(builder);
-				State.Container = builder.Build();
-			}
+			});
+		});
 
-			_injector?.Source(State.Container);
-		}
-
-		private void register(ContainerBuilder builder, Func<ContainerAdaptor> adaptor)
+		if (State.Container == null)
 		{
-			builder.RegisterInstance(State.TestDoubles).ExternallyOwned();
-
-			var containerAdaptor = adaptor.Invoke();
-
-			var context = new ContainerSetupContext(containerAdaptor, _extensions);
-			context.AddService(this);
-
-			InvokeExtensions<IContainerSetup>(x => x.ContainerSetup(context));
-			InvokeExtensions<IExtendSystem>(x => x.Extend(context));
-			InvokeExtensions<IIsolateSystem>(x => x.Isolate(context));
+			var builder = new ContainerBuilder();
+			register(builder, () => new ContainerAdaptor(new IgnoreTestDoubles(), builder, _extensions));
+			State.TestDoubles.RegisterFromPreviousContainer(builder);
+			State.Container = builder.Build();
 		}
 
-		public void AfterTest(ITest testDetails)
-		{
-			InvokeExtensions<ITestTeardown>(x => x.TestTeardown());
-			disposeContainer();
-			(State.TestDoubles as IDisposable)?.Dispose();
-			_injector = null;
-			_state.TryRemove(WorkerId, out _);
-		}
+		_injector?.Source(State.Container);
+	}
 
-		private void disposeContainer()
-		{
-			State.Container?.Dispose();
-		}
+	private void register(ContainerBuilder builder, Func<ContainerAdaptor> adaptor)
+	{
+		builder.RegisterInstance(State.TestDoubles).ExternallyOwned();
 
-		protected void InvokeExtensions<T>(Action<T> action) where T : class =>
-			_extensions.InvokeExtensions(action);
+		var containerAdaptor = adaptor.Invoke();
+
+		var context = new ContainerSetupContext(containerAdaptor, _extensions);
+		context.AddService(this);
+
+		InvokeExtensions<IContainerSetup>(x => x.ContainerSetup(context));
+		InvokeExtensions<IExtendSystem>(x => x.Extend(context));
+		InvokeExtensions<IIsolateSystem>(x => x.Isolate(context));
+	}
+
+	public void AfterTest(ITest testDetails)
+	{
+		InvokeExtensions<ITestTeardown>(x => x.TestTeardown());
+		disposeContainer();
+		(State.TestDoubles as IDisposable)?.Dispose();
+		_injector = null;
+		_state.TryRemove(WorkerId, out _);
+	}
+
+	private void disposeContainer() => 
+		State.Container?.Dispose();
+
+	protected void InvokeExtensions<T>(Action<T> action) where T : class =>
+		_extensions.InvokeExtensions(action);
 		
-		protected IEnumerable<T> QueryAllAttributes<T>() => _extensions.Query<T>();
-		protected T Resolve<T>() => State.Container.Resolve<T>();
+	protected IEnumerable<T> QueryAllAttributes<T>() => _extensions.Query<T>();
+	protected T Resolve<T>() => State.Container.Resolve<T>();
 
-		public void SimulateShutdown() => disposeContainer();
+	public void SimulateShutdown() => 
+		disposeContainer();
 
-		public void SimulateRestart()
-		{
-			disposeContainer();
-			rebuildContainer();
-			_injector?.Inject();
-		}
+	public void SimulateRestart()
+	{
+		disposeContainer();
+		rebuildContainer();
+		_injector?.Inject();
 	}
 }
