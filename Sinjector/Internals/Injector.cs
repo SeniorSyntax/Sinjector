76b48585b2918a88f1651757b3c76faffdf8653a
Roger Kratz <roger.kratz@gmail.com>
2025-08-15 14:47:38 +0200

    some cleanup

--- Sinjector/Internals/Injector.cs
+++ Sinjector/Internals/Injector.cs
@@ -3,51 +3,47 @@ using System.Linq;
 using System.Reflection;
 using Autofac;
 
-namespace Sinjector.Internals
+namespace Sinjector.Internals;
+
+internal class Injector
 {
-	internal class Injector
+	private IComponentContext _container;
+	private readonly List<object> _targets = new();
+
+	public Injector Source(IComponentContext container)
 	{
-		private IComponentContext _container;
-		private readonly List<object> _targets = new List<object>();
+		_container = container;
+		return this;
+	}
 
-		public Injector Source(IComponentContext container)
-		{
-			_container = container;
-			return this;
-		}
+	public Injector Target(object target)
+	{
+		_targets.Add(target);
+		return this;
+	}
 
-		public Injector Target(object target)
-		{
-			_targets.Add(target);
-			return this;
-		}
+	public void Inject() => 			
+		_targets.ForEach(x => injectTo(_container, x));
+		
+	private static void injectTo(IComponentContext container, object target)
+	{
+		var type = target.GetType();
+		var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+			.Where(x => x.CanWrite);
+		properties.ForEach(x => x.SetValue(target, container.Resolve(x.PropertyType), null));
+		var fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance);
+		fields.ForEach(x => x.SetValue(target, container.Resolve(x.FieldType)));
+	}
 
-		public void Inject() => InjectFrom(_container);
-
-		//can we remove this one?
-		public void InjectFrom(IComponentContext container) =>
-			_targets.ForEach(x => injectTo(container, x));
-
-		private static void injectTo(IComponentContext container, object target)
-		{
-			var type = target.GetType();
-			var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
-				.Where(x => x.CanWrite);
-			properties.ForEach(x => x.SetValue(target, container.Resolve(x.PropertyType), null));
-			var fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance);
-			fields.ForEach(x => x.SetValue(target, container.Resolve(x.FieldType)));
-		}
-
-		internal static void Inject(object target, object instance)
-		{
-			var type = target.GetType();
-			var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
-				.Where(x => x.CanWrite)
-				.Where(x => x.PropertyType.IsInstanceOfType(instance));
-			properties.ForEach(x => x.SetValue(target, instance, null));
-			var fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance)
-				.Where(x => x.FieldType.IsInstanceOfType(instance));
-			fields.ForEach(x => x.SetValue(target, instance));
-		}
+	internal static void Inject(object target, object instance)
+	{
+		var type = target.GetType();
+		var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+			.Where(x => x.CanWrite)
+			.Where(x => x.PropertyType.IsInstanceOfType(instance));
+		properties.ForEach(x => x.SetValue(target, instance, null));
+		var fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance)
+			.Where(x => x.FieldType.IsInstanceOfType(instance));
+		fields.ForEach(x => x.SetValue(target, instance));
 	}
 }
