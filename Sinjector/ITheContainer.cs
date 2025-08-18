feccf912459fbae00ded4e05e79e4c9a3d557b88
Roger Kratz <roger.kratz@gmail.com>
2025-08-18 15:57:29 +0200

    removing not needed Build method on ITheContainerBuilder

--- Sinjector/ITheContainer.cs
+++ Sinjector/ITheContainer.cs
@@ -7,10 +7,7 @@ public interface ITheContainerBuilder
 {
     //should be removed
     ContainerBuilder ContainerBuilder { get; }
-    
-    
-    //can be removed?
-    ITheContainer Build();
+
     void RegisterTestDoubleType(Type type, Type[] asTypes);
     void RegisterTestDoubleInstance(object instance, Type[] asTypes);
 }
