e5cbda771c728746fe9bb126db321a00b056bf12
Roger Kratz <roger.kratz@gmail.com>
2025-08-22 15:21:15 +0200

    aspnetcore tests need to be seperate based on ioc type

---
+++ Sinjector.Test/AspNetCore_Net/RememberToAddTestHereTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Sinjector.Test.AspNetCore;
+
+public class RememberToAddTestHereTest
+{
+    [Ignore("to be fixed")]
+    [Test]
+    public void Seomthing()
+    {
+        
+    }
+}
