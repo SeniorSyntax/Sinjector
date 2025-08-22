9e170f93d5917a13eabc710b6586b2f7ba5b7f17
Roger Kratz <roger.kratz@gmail.com>
2025-08-22 14:58:06 +0200

    adding a autofac module test

---
+++ Sinjector.Test/AutofacModuleTest.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace Sinjector.Test;
+
+[SinjectorFixture]
+public class AutofacModuleTest : IExtendSystem
+{
+    public TheService Target;
+
+    [Test]
+    public void ShouldCallService()
+    {
+        Target.Return33()
+            .Should().Be.EqualTo(33);
+    }
+    
+    public void Extend(IExtend extend) => 
+        extend.AddModule(new MyModule());
+
+    public class MyModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<TheService>();
+        }
+    }
+
+    public class TheService
+    {
+        public int Return33()
+        {
+            return 33;
+        }
+    }
+}
