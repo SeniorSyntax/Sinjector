f04ea9c6938d937a2887d34ea1e6dfe894aaf1e1
Roger Kratz <roger.kratz@gmail.com>
2025-08-25 14:13:52 +0200

    ITestDoubles can now be internal again

--- Sinjector/Internals/ITestDoubles.cs
+++ Sinjector/Internals/ITestDoubles.cs
@@ -2,9 +2,7 @@ using System;
 
 namespace Sinjector.Internals;
 
-
-//todo: back to internal?
-public interface ITestDoubles
+internal interface ITestDoubles
 {
     void Register(ITheContainerBuilder builder, object instance, Type type, Type[] asTypes);
     void RegisterFromPreviousContainer(ITheContainerBuilder builder);
