d4843b3689bb3b0d29881a8276ad629a15a2ea27
Roger Kratz <roger.kratz@gmail.com>
2025-08-25 13:44:00 +0200

    one type per file

---
+++ Sinjector/ITheContainerBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Sinjector.Internals;
+
+namespace Sinjector;
+
+public interface ITheContainerBuilder
+{
+    void RegisterTestDoubleType(Type type, Type[] asTypes, ITestDoubles testDoubles);
+    void RegisterTestDoubleInstance(object instance, Type[] asTypes);
+    object AddService(Type type, bool instancePerLifeTimeScope);
+    void AddService<TService>(TService instance) where TService : class;
+    void Add(object thing);
+    ITheContainer Build();
+}
