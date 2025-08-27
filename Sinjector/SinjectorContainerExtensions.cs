c602cd4438ab1563dfbf14a608d8d11e94fc4977
Roger Kratz <roger.kratz@gmail.com>
2025-08-27 11:42:39 +0200

    file rename

--- Sinjector/SinjectorContainerExtensions.cs
+++ Sinjector/SinjectorContainerExtensions.cs
@@ -1,6 +1,6 @@
 ﻿namespace Sinjector;
 
-public static class TheContainerExtensions
+public static class SinjectorContainerExtensions
 {
     public static T Resolve<T>(this ISinjectorContainer container) => 
         (T)container.Resolve(typeof(T));
