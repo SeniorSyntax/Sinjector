f79795586a33d19809115f6db49201e5a00f24ac
Roger Kratz <roger.kratz@gmail.com>
2025-08-29 15:11:28 +0200

    allow interface dep in NetIocContainer instead

--- Sinjector.Net/NetIocContainer.cs
+++ Sinjector.Net/NetIocContainer.cs
@@ -1,13 +1,12 @@
 using System;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Sinjector;
 
-public class NetIocContainer(ServiceProvider serviceProvider) : ISinjectorContainer
+public class NetIocContainer(IServiceProvider serviceProvider) : ISinjectorContainer
 {
     public object Resolve(Type type) => 
         serviceProvider.GetService(type);
 
     public void Dispose() => 
-        serviceProvider.Dispose();
+        (serviceProvider as IDisposable)?.Dispose();
 }
