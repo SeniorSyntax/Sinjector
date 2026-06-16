258cf17c7eace0bb8b28c94fe121480d2fed8a70
Roger Kratz <roger.kratz@gmail.com>
2026-06-16 18:28:29 +0200

    minor refact - using some hip new syntax for extension methods

--- Sinjector/Internals/SinjectorContainerBuilderExtensions.cs
+++ Sinjector/Internals/SinjectorContainerBuilderExtensions.cs
@@ -4,9 +4,12 @@ namespace Sinjector.Internals;
 
 internal static class SinjectorContainerBuilderExtensions
 {
-    internal static void AddService(this ISinjectorContainerBuilder sinjectorContainerBuilder, Type type) => 
-        sinjectorContainerBuilder.RegisterTestDoubleType(type, type.GetInterfaces());
+    extension(ISinjectorContainerBuilder sinjectorContainerBuilder)
+    {
+        internal void AddService(Type type) => 
+            sinjectorContainerBuilder.RegisterTestDoubleType(type, type.GetInterfaces());
 
-    internal static void AddService<TService>(this ISinjectorContainerBuilder sinjectorContainerBuilder, TService instance) where TService : class => 
-        sinjectorContainerBuilder.RegisterTestDoubleInstance(instance, instance.GetType().GetInterfaces());
+        internal void AddService<TService>(TService instance) where TService : class => 
+            sinjectorContainerBuilder.RegisterTestDoubleInstance(instance, instance.GetType().GetInterfaces());
+    }
 }
